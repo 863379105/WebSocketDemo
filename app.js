0807ac423e75a1f61bf56d3c00fdd4c5f047a4c3
LB <lb_space@LBdeMacBook-Pro.local>
2020-11-08 11:29:43 +0800

    complete front

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 //引入模块
 const Koa = require('koa')
+const Router = require('@koa/router')
 const socketIo = require('socket.io')
+const fs = require('fs')
 
 //实例化koa对象
 const app = new Koa()
 //初始化sokcet
 const server = require('http').createServer(app.callback());//因为koa没有暴露server对象，这里很巧妙的运用了server做了一个代理
 const io = socketIo(server)
+//实例化router对象
+const router = new Router()
+
+//配置路由
+router.get('/',async ctx => {
+    ctx.type = 'text/html;charset=utf-8'
+    ctx.body = fs.readFileSync('./index.html')
+})
+app.use(router.routes())
 
 io.on('connection',() => {
-
+    console.log('a user connected');
 })
 
 //监听服务端口
